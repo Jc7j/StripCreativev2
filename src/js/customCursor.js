ac80eb7ae481fa414b82a2fe112ca27c25beded3
Jason Chiang <chiangjason19@gmail.com>
2024-06-06 10:56:48 -0700

    didnt fix but trying this one out

--- src/js/customCursor.js
+++ src/js/customCursor.js
@@ -1,50 +1,50 @@
 export function cursor() {
   if (window.innerWidth > 1000) {
-  const cursor = document.querySelector(".cursor");
-  const moveCursor = (e) =>
-    gsap.to(cursor, {
-      duration: 0.3,
-      x: e.clientX,
-      y: e.clientY,
-      ease: "Power1.out",
-    });
+    const cursor = document.querySelector(".cursor");
+    const moveCursor = (e) =>
+      gsap.to(cursor, {
+        duration: 0.3,
+        x: e.clientX,
+        y: e.clientY,
+        ease: "Power1.out",
+      });
 
-  document.addEventListener("mousemove", moveCursor);
-  document.addEventListener("mouseenter", () =>
-    gsap.to(cursor, { duration: 0.3, scale: 1, ease: "Power1.out" }),
-  );
-  document.addEventListener("mouseleave", () =>
-    gsap.to(cursor, { duration: 0.3, scale: 0, ease: "Power1.out" }),
-  );
+    document.addEventListener("mousemove", moveCursor);
+    document.addEventListener("mouseenter", () =>
+      gsap.to(cursor, { duration: 0.3, scale: 1, ease: "Power1.out" }),
+    );
+    document.addEventListener("mouseleave", () =>
+      gsap.to(cursor, { duration: 0.3, scale: 0, ease: "Power1.out" }),
+    );
 
-  const applyCursorEffect = (elements, scale, filter = "blur(0px)") => {
-    elements.forEach((el) => {
-      el.addEventListener("mouseenter", () =>
-        gsap.to(cursor, {
-          duration: 0.3,
-          scale,
-          ease: "Power1.out",
-          filter: "blur(2px)",
-        }),
-      );
-      el.addEventListener("mouseleave", () =>
-        gsap.to(cursor, {
-          duration: 0.3,
-          scale: 1,
-          ease: "Power1.out",
-          filter,
-        }),
-      );
-    });
-  };
+    const applyCursorEffect = (elements, scale, filter = "blur(0px)") => {
+      elements.forEach((el) => {
+        el.addEventListener("mouseenter", () =>
+          gsap.to(cursor, {
+            duration: 0.3,
+            scale,
+            ease: "Power1.out",
+            filter: "blur(2px)",
+          }),
+        );
+        el.addEventListener("mouseleave", () =>
+          gsap.to(cursor, {
+            duration: 0.3,
+            scale: 1,
+            ease: "Power1.out",
+            filter,
+          }),
+        );
+      });
+    };
 
-  const links = document.querySelectorAll(".focus");
-  const headings = document.querySelectorAll(".focus2");
+    const links = document.querySelectorAll(".focus");
+    const headings = document.querySelectorAll(".focus2");
 
-  applyCursorEffect(links, 5);
-  applyCursorEffect(headings, 15);
-} else {
-  // Optionally, you can log a message or handle the mobile case differently here
-  console.log("Sticky scroll animations disabled on mobile devices.");
-}
+    applyCursorEffect(links, 5);
+    applyCursorEffect(headings, 15);
+  } else {
+    // Optionally, you can log a message or handle the mobile case differently here
+    console.log("Sticky scroll animations disabled on mobile devices.");
+  }
 }
