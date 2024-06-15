88b1ea17323ebc20fc524bf8e6a01d9ce684f567
Jason Chiang <chiangjason19@gmail.com>
2024-06-14 19:23:10 -0700

    change availability font-size on mobile

--- src/pages/components/Availability.jsx
+++ src/pages/components/Availability.jsx
@@ -10,7 +10,7 @@ export default function Availability() {
       >
         <circle cx="10" cy="10" r="10" fill="red" />
       </svg>
-      <span className="ml-2 font-medium text-lg">
+      <span className="ml-2 font-medium md:text-xl text-body">
         2 spots available left - Starts at $5k/mo
       </span>
     </div>
